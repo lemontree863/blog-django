d1bea9ecb2ffaf8240510bce27c938c5474bbabb
yuefengyang <yfyang@mytijian.com>
2019-11-19 14:54:58 +0800

    allow host

--- blogprohect/settings.py
+++ blogprohect/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l6fz72#cf01twzjtgf=l^r9xar60t=rmdrp7ge_15t_^+&_&5('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost ']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '116.62.160.157']
 
 
 # Application definition
