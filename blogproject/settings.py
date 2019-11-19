3dfd07e35e66a6d75c907bc72d3e853d6c672a1f
yuefengyang <yfyang@mytijian.com>
2019-11-19 19:43:14 +0800

    debug false

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l6fz72#cf01twzjtgf=l^r9xar60t=rmdrp7ge_15t_^+&_&5('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '116.62.160.157']
 
